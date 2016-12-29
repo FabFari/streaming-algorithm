2d04485b6244f9194c93ae75146babcfc2883c75
FabFari <fabriziofarinacci@gmail.com>
2016-12-29 18:46:31 +0100

    Added Alon Matias Szegedy real time v1

--- alon_matias_szegedy.py
+++ alon_matias_szegedy.py
@@ -39,5 +39,29 @@ def alon_matias_szegedy(num_hashes=64, filename=STREAM_FILENAME, debug=False):
 
     return float(sum_est) / float(num_hashes)
 
+
+def alon_matias_szegedy_real_time(line, estimates, avg=0.0, num_hashes=64, filename=STREAM_FILENAME, debug=False):
+    new_estimates = [0 for j in range(num_hashes)]
+
+    for i in range(num_hashes):
+        new_estimates[i] = 1 if pymmh3.hash(line, seed=i) % 2 == 0 else -1
+
+    if debug:
+        print new_estimates
+
+    # estimates_n-1[i] = (a_1 + a_2 + ... + a_n-1)^2
+    # estimates_n[i] = (a_1 + a_2 + ... + a_n-1 + a_n)^2 = (srqt(estimates_n-1[i]) + a_n)^2 =
+    #                = estimates_n-1[i] + estimates_n[i] + 2 * estimates_n-1[i] * estimates_n[i]
+    # for i in range(num_hashes):
+    #     estimates[i] = estimates[i] ** 2 + new_estimates[i] ** 2 + 2 * estimates[i] * new_estimates[i]
+
+    # if debug:
+    #     print estimates
+
+    for i in range(num_hashes):
+        avg += float(new_estimates[i] ** 2 + 2 * estimates[i] * new_estimates[i]) / float(num_hashes)
+
+    return estimates, avg
+
 if __name__ == "__main__":
     print alon_matias_szegedy()
